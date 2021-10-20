67d525bbd8840c3f6578e949e20665b7c0841cca
wilsonbljr <wilsonbljr@gmail.com>
2021-10-19 21:37:40 -0300

    Adds scroll event listener to "Saiba Mais"

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,14 +1,15 @@
 // Open and close menu variables
-var openButton = document.querySelector('#openMenu');
-var closeButton = document.querySelector('#closeMenu');
-var hamburgerMenu = document.querySelector('.hamburger');
+let openButton = document.querySelector('#openMenu');
+let closeButton = document.querySelector('#closeMenu');
+let hamburgerMenu = document.querySelector('.hamburger');
 
 //Navigation variables
-var home = document.querySelector('#presentation');
-var about = document.querySelector('#about');
-var skills = document.querySelector('#skills');
-var projects = document.querySelector('#projects');
-var contact = document.querySelector('#contact');
+let home = document.querySelector('#presentation');
+let about = document.querySelector('#about');
+let skills = document.querySelector('#skills');
+let projects = document.querySelector('#projects');
+let contact = document.querySelector('#contact');
+let scrollDown = document.querySelector('.presentation__scrollDown')
 
 //Listeners to open and close menu buttons
 openButton.addEventListener('click', function () {openMenu()});
@@ -20,6 +21,8 @@ document.querySelector('#item__sobre').addEventListener('click', function () {na
 document.querySelector('#item__skills').addEventListener('click', function () {nav(skills)});
 document.querySelector('#item__projetos').addEventListener('click', function () {nav(projects)});
 document.querySelector('#item__contato').addEventListener('click', function () {nav(contact)});
+scrollDown.addEventListener('click', function () {nav(about)});
+window.addEventListener('scroll', function () {hideScrollDown()});
 
 //Functions to open and close the menu
 function openMenu() {
@@ -40,3 +43,14 @@ function nav(where) {
     window.scrollTo({ top: where.offsetTop, behavior: 'smooth'});
     closeMenu();
 };
+
+//Function to hide scrollDown ("Saiba mais")
+function hideScrollDown () {
+    let y = window.scrollY;
+    if (y > 0) {
+        scrollDown.classList.add("scrollDown__hide");
+        console.log(y);
+    } else {
+        scrollDown.classList.remove("scrollDown__hide");
+    };
+}
